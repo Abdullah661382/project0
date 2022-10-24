2f8affd767b5dc47c9959e4c8f34d078f99992b3
Abdullah661382 <AbdullahAdel711@gmail.com>
2022-10-24 14:41:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Hello World") # this is a comment
+print((12.4*5.9)/(3.7*2.5)) # this is a calculation
+print((42.3+18.4)/(16.7*0.4)) # this is another calculation
+print((81.73-21.4)/(3.87*1.25)) # more calculations
+print(30.5**0.5) # 0.5 is the same as square root
+print((1.00/9.81)**0.5) # no idea what to call this
